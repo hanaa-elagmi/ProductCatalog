f44f2913153844f3b4b6f543d4153d0e196ec92d
hanaa-elagmi <hanaa.elagmi@gmail.com>
2025-03-04 02:43:06 +0200

    Add project files.

---
+++ ProductCatalog/Controllers/Account/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProductCatalog.Models;
+using ProductCatalog.ViewModel;
+
+namespace ProductCatalog.Controllers.Account
+{
+	public class AccountController : Controller
+	{
+		private readonly UserManager<ApplicationUser> userManager;
+		private readonly SignInManager<ApplicationUser> signInManager;
+
+		public AccountController(UserManager<ApplicationUser>userManager,SignInManager<ApplicationUser> signInManager)
+        {
+			this.userManager = userManager;
+			this.signInManager = signInManager;
+		}
+        public IActionResult Registeration()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Registeration(RegistrationViewModel newUser)
+		{
+			if (ModelState.IsValid) 
+			{
+				ApplicationUser user = new ApplicationUser();
+				user.UserName = newUser.UserName;
+				user.Email = newUser.Email;
+				user.Address = newUser.Address; 
+				user.PasswordHash = newUser.Password;
+				IdentityResult result=await userManager.CreateAsync(user,newUser.Password);
+
+				if (result.Succeeded) 
+				{
+				//create cookie
+					await signInManager.SignInAsync(user, isPersistent: false);
+					return RedirectToAction("Index", "Home");
+				}
+				else {
+					//some issue===>sent it to user
+                    foreach (var item in result.Errors)
+                    {
+						ModelState.AddModelError("", item.Description);
+                    }
+                }
+			}
+			return View();
+		}
+	}
+}
---
+++ ProductCatalog/ViewModel/RegistrationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductCatalog.ViewModel
+{
+	public class RegistrationViewModel
+	{
+		[Required]
+		public string UserName { get; set; }
+		[DataType(DataType.Password)]
+		[Required]
+		public string Password { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare("Password")]
+		public string ConfirmPassword { get; set; }
+		[Required]
+		public string Email { get; set; }
+		[Required]
+		public string PhoneNumber { get; set; }
+		public string? Address { get; set; }
+
+	}
+}
